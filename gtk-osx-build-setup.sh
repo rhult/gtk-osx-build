11f3a27517ddd97ac598b1c6675595f3de3147fc
Richard Hult <richard@imendio.com>
2008-05-17 19:50:39 +0200

    Commit to try the hook

--- gtk-osx-build-setup.sh
+++ gtk-osx-build-setup.sh
@@ -15,27 +15,12 @@ fi
 echo "Installing jhbuild..."
 (cd jhbuild && make -f Makefile.plain DISABLE_GETTEXT=1 install >/dev/null)
 
-echo "Checking out gtk-osx build setup from subversion..."
-if ! test -d $SOURCE/gtk-osx-build; then
-    svn co http://developer.imendio.com/svn/gtk-osx-build >/dev/null
-else
-    (cd gtk-osx-build && svn up >/dev/null)
-fi
-
-echo "Installing gtk-osx build setup..."
-(cd gtk-osx-build; ln -sfh `pwd`/jhbuildrc-gtk-osx $HOME/.jhbuildrc)
-(cd gtk-osx-build; ln -sfh `pwd`/jhbuildrc-gtk-osx-fw-10.4 $HOME/.jhbuildrc-fw-10.4)
-if [ ! -f $HOME/.jhbuildrc-custom ]; then
-    (cd gtk-osx-build; cp jhbuildrc-gtk-osx-custom-example $HOME/.jhbuildrc-custom)
-fi
-
-if test "x$1" == "xmaintainer-mode"; then
-    echo "Setting up extra modulesets for maintainer mode..."
-    cd gtk-osx-build
-    for f in `ls *modules`; do
-        ln -sfh `pwd`/$f $SOURCE/jhbuild/modulesets/
-    done
-    cd ..
-fi
+echo "Downloading and installing gtk-osx jhbuild setup..."
+#curl -O ...
+#(cd gtk-osx-build; ln -sfh `pwd`/jhbuildrc-gtk-osx $HOME/.jhbuildrc)
+(#cd gtk-osx-build; ln -sfh `pwd`/jhbuildrc-gtk-osx-fw-10.4 $HOME/.jhbuildrc-fw-10.4)
+#if [ ! -f $HOME/.jhbuildrc-custom ]; then
+#    (cd gtk-osx-build; cp jhbuildrc-gtk-osx-custom-example $HOME/.jhbuildrc-custom)
+#fi
 
 echo "Done."
